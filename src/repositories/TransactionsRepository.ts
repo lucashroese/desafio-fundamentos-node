a081588003b6751911bfcdb817c751063aaed9a5
Lucas Roese <lucashroese@gmail.com>
2020-05-23 17:14:08 -0300

    Desafio Fundamentos NodeJS

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const transactions = this.all();
+    const income = transactions.reduce((accumulator, currentValue) => {
+      if (currentValue.type === 'income') {
+        return accumulator + currentValue.value;
+      }
+      return accumulator;
+    }, 0);
+    const outcome = transactions.reduce((accumulator, currentValue) => {
+      if (currentValue.type === 'outcome') {
+        return accumulator + currentValue.value;
+      }
+      return accumulator;
+    }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
